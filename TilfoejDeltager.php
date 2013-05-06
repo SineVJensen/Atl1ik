8b26fe8fee923300cf48d8f37d2eb86e6a2ab563
SineVJensen <sine144@gmail.com>
2013-05-06 21:42:56 +0200

    Nu med korrekt style

--- TilfoejDeltager.php
+++ TilfoejDeltager.php
@@ -20,20 +20,39 @@ $query3 = "SELECT * FROM distancer";
 $result3 = mysql_query($query3);
 ?>
 
-<html>
-  <head>
-		<title>Tilfoej Deltager</title>
-	</head>
+<HTML xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+
+	<head profile="http://gmpg.org/xfn/11">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>TilmeldingBaneturnering</title>
+
+		<link rel="stylesheet" href="http://www.baneturneringen.dk/wp-content/themes/default/reset.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="http://www.baneturneringen.dk/wp-content/themes/default/style.css" type="text/css" media="screen" />
+		<link rel="pingback" href="http://www.baneturneringen.dk/xmlrpc.php" />
+
+		<link rel="alternate" type="application/rss+xml" title="Baneturneringen &raquo; Hjem Comments Feed" href="http://www.baneturneringen.dk/hjem-2/feed/" />
+		<script type='text/javascript' src='http://www.baneturneringen.dk/wp-includes/js/jquery/jquery.js?ver=1.4.2'></script>
+		<script type='text/javascript' src='http://www.baneturneringen.dk/wp-includes/js/comment-reply.js?ver=20090102'></script>
+		<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.baneturneringen.dk/xmlrpc.php?rsd" />
+		<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.baneturneringen.dk/wp-includes/wlwmanifest.xml" /> 
+		<link rel='index' title='Baneturneringen' href='http://www.baneturneringen.dk/' />
+		<link rel='prev' title='21.-22. august' href='http://www.baneturneringen.dk/resultater/21-22-august/' />
+		<link rel='next' title='Runder 2012' href='http://www.baneturneringen.dk/runder2012/runder-2012/' />
+		<meta name="generator" content="WordPress 3.0" />
+		<link rel='canonical' href='http://www.baneturneringen.dk/' />
+		<style type="text/css" media="all">
+		/* <![CDATA[ */
+			@import url("http://www.baneturneringen.dk/wp-content/plugins/wp-table-reloaded/css/plugin.css?ver=1.7");
+			@import url("http://www.baneturneringen.dk/wp-content/plugins/wp-table-reloaded/css/datatables.css?ver=1.7");
+		/* ]]> */
+
+	</style></head>
 	
-	<h1>
-		<input name="Tilbage" type="button" value="Tilbage" />&nbsp;<input name="Videre" type="submit" value="Videre" />
-	</h1>
-	<div style="text-align: center;">
+
 		<h1>
 			Tilmeld deltager
 		</h1>
-	</div>
-	<hr />
+
 	<br />
 	<center>
 	<!-- sender de indtastede info'er videre til deltagerliste som indsætter det i databasen -->
